6b36e8ae7653fd8f8957a7d68c2dd2288f83c509
pedro martelletto <pedro@ambientworks.net>
2017-01-14 10:38:46 +0000

    Return filter in handleGetCBFilter()

--- rpcserver.go
+++ rpcserver.go
@@ -2138,7 +2138,7 @@ func handleGetCBFilter(s *rpcServer, cmd interface{}, closeChan <-chan struct{})
 		}
 	}
 
-	return nil, nil
+	return hex.EncodeToString(filterBytes), nil
 }
 
 // handleGetConnectionCount implements the getconnectioncount command.
