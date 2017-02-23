b02dd5fe274dccdc533cefc76e3446324eae3f76
Alex <alex@akselrod.org>
2017-02-22 19:30:53 -0700

    Rename CFilterHeader to CFHeader on P2P side; fix some bugs/tests

--- wire/message.go
+++ wire/message.go
@@ -28,7 +28,6 @@ const MaxMessagePayload = (1024 * 1024 * 32) // 32MB
 
 // Commands used in bitcoin message headers which describe the type of message.
 const (
-<<<<<<< HEAD
 	CmdVersion                   = "version"
 	CmdVerAck                    = "verack"
 	CmdGetAddr                   = "getaddr"
@@ -53,7 +52,9 @@ const (
 	CmdSendHeaders               = "sendheaders"
 	CmdFeeFilter                 = "feefilter"
 	CmdGetCFilter                = "getcfilter"
+	CmdGetCFHeader               = "getcfheader"
 	CmdCFilter                   = "cfilter"
+	CmdCFHeader                  = "cfheader"
 	CmdMasternodeWinner          = "mnw"
 	CmdMasternodeProposal        = "mprop"
 	CmdMasternodeVote            = "mvote"
@@ -73,35 +74,6 @@ const (
 	CmdFinalizedBudgetVote       = "fbvote"
 	CmdSyncStatusCount           = "ssc"
 	CmdMasternodeVoteSync        = "mnvs"
-=======
-	CmdVersion           = "version"
-	CmdVerAck            = "verack"
-	CmdGetAddr           = "getaddr"
-	CmdAddr              = "addr"
-	CmdGetBlocks         = "getblocks"
-	CmdInv               = "inv"
-	CmdGetData           = "getdata"
-	CmdNotFound          = "notfound"
-	CmdBlock             = "block"
-	CmdTx                = "tx"
-	CmdGetHeaders        = "getheaders"
-	CmdHeaders           = "headers"
-	CmdPing              = "ping"
-	CmdPong              = "pong"
-	CmdAlert             = "alert"
-	CmdMemPool           = "mempool"
-	CmdFilterAdd         = "filteradd"
-	CmdFilterClear       = "filterclear"
-	CmdFilterLoad        = "filterload"
-	CmdMerkleBlock       = "merkleblock"
-	CmdReject            = "reject"
-	CmdSendHeaders       = "sendheaders"
-	CmdFeeFilter         = "feefilter"
-	CmdGetCFilter        = "getcfilter"
-	CmdGetCFilterHeader  = "getcfilterheader"
-	CmdCFilter           = "cfilter"
-	CmdCFilterHeader     = "cfilterheader"
->>>>>>> b8c3be74... Add CFilterHeader p2p counterparts
 )
 
 // MessageEncoding represents the wire message encoding format to be used.
@@ -210,13 +182,15 @@ func makeEmptyMessage(command string) (Message, error) {
 	case CmdGetCFilter:
 		msg = &MsgGetCFilter{}
 
-	case CmdGetCFilterHeader:
-		msg = &MsgGetCFilterHeader{}
+	case CmdGetCFHeader:
+		msg = &MsgGetCFHeader{}
 
 	case CmdCFilter:
 		msg = &MsgCFilter{}
 
-<<<<<<< HEAD
+	case CmdCFHeader:
+		msg = &MsgCFHeader{}
+
 	case CmdMasternodeWinner:
 		msg = &MsgMasternodeWinner{}
 
@@ -231,10 +205,6 @@ func makeEmptyMessage(command string) (Message, error) {
 
 	case CmdSyncStatusCount:
 		msg = &MsgSyncStatusCount{}
-=======
-	case CmdCFilterHeader:
-		msg = &MsgCFilterHeader{}
->>>>>>> b8c3be74... Add CFilterHeader p2p counterparts
 
 	default:
 		return nil, fmt.Errorf("unhandled command [%s]", command)
