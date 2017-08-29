41722c3b880904193f57efe9e532c5b295e3d74b
Alex <alex@akselrod.org>
2017-08-29 16:04:04 -0600

    server: init rpcserver with cfindex

--- server.go
+++ server.go
@@ -2545,6 +2545,7 @@ func newServer(listenAddrs []string, db database.DB, chainParams *chaincfg.Param
 			CPUMiner:    s.cpuMiner,
 			TxIndex:     s.txIndex,
 			AddrIndex:   s.addrIndex,
+			CfIndex:     s.cfIndex,
 		})
 		if err != nil {
 			return nil, err
