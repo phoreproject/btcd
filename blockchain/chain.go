92c2dc549fb5dd9d30d8a269b8076833670f42d1
Olaoluwa Osuntokun <laolu32@gmail.com>
2018-08-08 18:30:46 -0700

    blockchain: mark blocks as invalid if they fail connectBlock

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -1121,6 +1121,17 @@ func (b *BlockChain) connectBestChain(node *blockNode, block *btcutil.Block, fla
 		// Connect the block to the main chain.
 		err := b.connectBlock(node, block, view, stxos)
 		if err != nil {
+			// If we got hit with a rule error, then we'll mark
+			// that status of the block as invalid and flush the
+			// index state to disk before returning with the error.
+			if _, ok := err.(RuleError); ok {
+				b.index.SetStatusFlags(
+					node, statusValidateFailed,
+				)
+			}
+
+			flushIndexState()
+
 			return false, err
 		}
 
